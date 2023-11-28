0859d92c2e67966ceaa379ed6dd7856cb7672ba4
StinkyJeans <johncona3@gmail.com>
2023-11-29 01:19:55 +0800

    updated the profile page where the profile in header cannot be clicked

--- src/pages/User/Profile.js
+++ src/pages/User/Profile.js
@@ -33,16 +33,11 @@ const ProfilePage = () => {
     });
   }, []);
 
-  const handleHomeClick = () => {
-    if (router.pathname !== '/') {
-      router.push('/User/Profile');
-    }
-  };
 
   return (   
     <div className=' xl:ml-[34%] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl mt-1'>
         <div className="justify-between flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200 items-center">
-          <h2 onClick={handleHomeClick} className="text-lg sm:text-xl font-bold cursor-pointer">Profile</h2>
+          <h2  className="text-lg sm:text-xl font-bold cursor-pointer">Profile</h2>
           <div className="ml-5 w-[80%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
             <div className="flex items-center p-3 rounded-full bg-red-300 relative">
               <SearchIcon className="h-5 z-50 text-gray-500" />
